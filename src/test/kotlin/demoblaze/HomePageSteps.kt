5cb16708d8ec03819397234d80a713ce71da5431
Joao Farias <joao@thatsabug.com>
2021-01-31 12:12:40 +0100

    Quick and dirty - 'select the Sony vaio i5 product in the Laptops category'

--- src/test/kotlin/demoblaze/HomePageSteps.kt
+++ src/test/kotlin/demoblaze/HomePageSteps.kt
@@ -4,6 +4,10 @@ import io.cucumber.java8.En
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
 import io.cucumber.java8.HookNoArgsBody
+import org.openqa.selenium.By
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait
+import java.time.Duration
 
 class HomePageSteps: En {
 
@@ -15,7 +19,18 @@ class HomePageSteps: En {
         Before(HookNoArgsBody { startBrowser() })
 
         Given("I am on the Demoblaze Home page") {
-            driver.get("https://www.demoblaze.com/index.html")
+            visitHomePage()
+        }
+
+        When("select the {string} product in the {string} category") {
+            productName: String, category: String ->
+            run {
+
+                selectCategory(category)
+                selectProduct(productName)
+
+            }
+
         }
 
         After(HookNoArgsBody { killBrowser() })
@@ -26,6 +41,30 @@ class HomePageSteps: En {
         options.setBinary("/usr/bin/brave")
         driver = ChromeDriver(options)
         driver.manage().window().maximize()
+    }
+
+    private fun visitHomePage() {
+        driver.get("https://www.demoblaze.com/index.html")
+    }
+
+    private fun selectCategory(category: String) {
+        val categories = driver.findElement(By.className("list-group"))
+        categories.findElement(By.ByXPath("//a[text()='$category']")).click()
+    }
+
+    private fun selectProduct(productName: String) {
+        val products = WebDriverWait(driver, Duration.ofSeconds(5)).until(
+            ExpectedConditions.presenceOfAllElementsLocatedBy((By.cssSelector("#tbodyid .card")))
+        )
+        val targetProduct = products.find { it.findElement(By.className("card-title")).text == productName }
+
+        targetProduct?.findElement(By.tagName("a"))?.click()
+        WebDriverWait(driver, Duration.ofSeconds(5)).until(
+            ExpectedConditions.elementToBeClickable((By.className("btn-success")))
+        ).click()
+        WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.alertIsPresent());
+        driver.switchTo().alert().accept()
+        visitHomePage()
     }
 
     private fun killBrowser() {
