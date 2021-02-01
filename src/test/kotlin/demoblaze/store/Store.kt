2d2b83776a116550e1012ce7b595b5db63de1cf6
Joao Farias <joao@thatsabug.com>
2021-02-01 21:16:24 +0100

    Cleaning - select the Sony vaio i5 product in the Laptops category - HomePage

--- src/test/kotlin/demoblaze/store/Store.kt
+++ src/test/kotlin/demoblaze/store/Store.kt
@@ -25,32 +25,16 @@ class Store(
 
     fun addProductToCart(productName: String, category: String) {
         selectCategory(category)
-        Thread.sleep(2000)
+        Thread.sleep(2500)
         selectProduct(productName)
     }
 
     private fun selectCategory(category: String) {
-        val categories = driver.findElement(By.className("list-group"))
-        categories.findElement(By.ByXPath("//a[text()='$category']")).click()
+        homePage.selectCategory(category)
     }
 
     private fun selectProduct(productName: String) {
-        val products = WebDriverWait(driver, Duration.ofSeconds(5)).until(
-            ExpectedConditions.presenceOfAllElementsLocatedBy((By.cssSelector("#tbodyid .card")))
-        )
-
-        val targetProduct = products.find {
-            WebDriverWait(driver, Duration.ofSeconds(5)).until(
-                ExpectedConditions.elementToBeClickable(it.findElement(By.className("card-title")))
-            ).text == productName
-        }
-
-        targetProduct?.findElement(By.tagName("a"))?.click()
-        WebDriverWait(driver, Duration.ofSeconds(5)).until(
-            ExpectedConditions.elementToBeClickable((By.className("btn-success")))
-        ).click()
-        WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.alertIsPresent());
-        driver.switchTo().alert().accept()
+        homePage.selectProduct(productName)
         this.visitHomePage()
     }
 }
--- src/test/kotlin/demoblaze/store/pages/home/HomePage.kt
+++ src/test/kotlin/demoblaze/store/pages/home/HomePage.kt
@@ -1,6 +1,10 @@
 package demoblaze.store.pages.home
 
+import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait
+import java.time.Duration
 
 class HomePage(private val driver: WebDriver) {
 
@@ -10,5 +14,29 @@ class HomePage(private val driver: WebDriver) {
 
     private fun openPage() {
         driver.get("https://www.demoblaze.com/index.html")
+    }
+
+    fun selectCategory(category: String) {
+        val categories = driver.findElement(By.className("list-group"))
+        categories.findElement(By.ByXPath("//a[text()='$category']")).click()
+    }
+
+    fun selectProduct(productName: String) {
+        val products = WebDriverWait(driver, Duration.ofSeconds(5)).until(
+            ExpectedConditions.presenceOfAllElementsLocatedBy((By.cssSelector("#tbodyid .card")))
+        )
+
+        val targetProduct = products.find {
+            WebDriverWait(driver, Duration.ofSeconds(5)).until(
+                ExpectedConditions.elementToBeClickable(it.findElement(By.className("card-title")))
+            ).text == productName
+        }
+
+        targetProduct?.findElement(By.tagName("a"))?.click()
+        WebDriverWait(driver, Duration.ofSeconds(5)).until(
+            ExpectedConditions.elementToBeClickable((By.className("btn-success")))
+        ).click()
+        WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.alertIsPresent());
+        driver.switchTo().alert().accept()
     }
 }
